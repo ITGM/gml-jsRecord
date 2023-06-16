f5e3c1b84dd23bac7ba84b060caad15f0b4412be
gm_l <18664383868@163.com>
2023-06-16 15:39:00 +0800

    feat: index 头像裁剪

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,21 @@
-<template>虚拟列表</template>
+<script setup>
+import avatar from '@/assets/images/avatar.png'
+const list = [avatar, avatar, avatar, avatar]
+const CropImg1 = await CropAvatar(list.slice(0, 1))
+const CropImg2 = await CropAvatar(list.slice(0, 2))
+const CropImg3 = await CropAvatar(list.slice(0, 3))
+const CropImg4 = await CropAvatar(list.slice(0, 4))
+</script>
+
+<template>
+  <p>头像裁剪重组</p>
+  <img :src="CropImg1" alt="" />
+  <img :src="CropImg2" alt="" />
+  <img :src="CropImg3" alt="" />
+  <img :src="CropImg4" alt="" />
+</template>
+<style>
+img {
+  margin: 20px;
+}
+</style>
